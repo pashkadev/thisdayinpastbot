81e9ddeb6f17d5e9e5958a36554baf71946f9fc4
pashkadev <44290120+pashkadev@users.noreply.github.com>
2018-10-31 17:15:44 +0300

    updates using pycharm

---
+++ bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import telebot
+import os
+import constant
+
+#main variables
+token = os.environ[constant.TELEGRAM_TOKEN]
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, "I'm up and runnig")
+
+
+bot.polling()
