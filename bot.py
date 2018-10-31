b18c1ce2d3be217cd1477210363b370b68eb676b
pashkadev <44290120+pashkadev@users.noreply.github.com>
2018-10-31 21:08:17 +0300

    reqs

--- bot.py
+++ bot.py
@@ -5,10 +5,11 @@ import os
 
 #main variables
 token = os.environ['TELEGRAM_TOKEN']
+#token = constant.TELEGRAM_TOKEN
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-    bot.send_message(message.chat.id, "I'm up and runnig")
+    bot.send_message(message.chat.id, "I'm up and running")
 
 
 bot.polling()
