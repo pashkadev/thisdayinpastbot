deadc680f2ceb2530b7cb500c2330b58a1fc97ce
pashkadev <44290120+pashkadev@users.noreply.github.com>
2018-11-01 16:54:01 +0300

    test ydisk

--- bot.py
+++ bot.py
@@ -24,9 +24,9 @@ y = yadisk.YaDisk(token=os.environ['YADISK_TOKEN'])#constant.YADISK_TOKEN)
 
 #@bot.message_handler(commands=['start', 'go'])
 #def start_handler(message):
-bot.send_message(chat_id='202906676', "I'm up and running")
-bot.send_message(chat_id='202906676', y.check_token())
-bot.send_message(chat_id='202906676', "https://yadi.sk/d/PJTlsclG3aTguX")
+bot.send_message(chat_id='202906676', text="I'm up and running")
+#bot.send_message(chat_id='202906676', texty.check_token())
+bot.send_message(chat_id='202906676', text="https://yadi.sk/d/PJTlsclG3aTguX")
 bot.send_photo(chat_id='202906676', photo='https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/robot.png')
 
 
