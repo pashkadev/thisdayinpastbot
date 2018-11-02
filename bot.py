62c7d27105450b49871976c0fe4881ea361aae8f
pashkadev <44290120+pashkadev@users.noreply.github.com>
2018-11-02 12:55:18 +0300

    test

--- bot.py
+++ bot.py
@@ -2,26 +2,24 @@
 import telebot
 from telebot import apihelper
 import os
-#import constant
+# import constant
 import yadisk
 import datetime
 
-#main variables
+# main variables
 token = os.environ['TELEGRAM_TOKEN']
 
-#apihelper.proxy = {'https':'socks5h://kmk.me.uk:3105'}
+# apihelper.proxy = {'https':'socks5h://kmk.me.uk:3105'}
 
-#token = constant.TELEGRAM_TOKEN
+# token = constant.TELEGRAM_TOKEN
 bot = telebot.TeleBot(token)
 
-y = yadisk.YaDisk(token=os.environ['YADISK_TOKEN'])#constant.YADISK_TOKEN)
+y = yadisk.YaDisk(token=os.environ['YADISK_TOKEN'])# constant.YADISK_TOKEN)
 
-print(datetime.datetime.now())
-#@bot.message_handler(commands=['start', 'go'])
-#def start_handler(message):
+# @bot.message_handler(commands=['start', 'go'])
+# def start_handler(message):
 bot.send_message(chat_id='202906676', text="I'm up and running")
-bot.send_message(chat_id='202906676', text="https://yadi.sk/d/PJTlsclG3aTguX")
-#bot.send_photo(chat_id='202906676', photo='https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/robot.png')
+bot.send_message(chat_id='202906676', text=str(datetime.datetime.now()))
 
 
 bot.polling()
