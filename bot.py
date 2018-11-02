042a8dcf2291a4bdf6290c6a62379149f81db4f7
pashkadev <44290120+pashkadev@users.noreply.github.com>
2018-11-02 10:15:32 +0300

    test

--- bot.py
+++ bot.py
@@ -16,18 +16,11 @@ bot = telebot.TeleBot(token)
 y = yadisk.YaDisk(token=os.environ['YADISK_TOKEN'])#constant.YADISK_TOKEN)
 
 
-
-#print(y.check_token())
-#print(list(y.listdir("/Photo_BACKUP")))
-
-
-
 #@bot.message_handler(commands=['start', 'go'])
 #def start_handler(message):
 bot.send_message(chat_id='202906676', text="I'm up and running")
-#bot.send_message(chat_id='202906676', texty.check_token())
 bot.send_message(chat_id='202906676', text="https://yadi.sk/d/PJTlsclG3aTguX")
-bot.send_photo(chat_id='202906676', photo='https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/robot.png')
+#bot.send_photo(chat_id='202906676', photo='https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/robot.png')
 
 
 bot.polling()
